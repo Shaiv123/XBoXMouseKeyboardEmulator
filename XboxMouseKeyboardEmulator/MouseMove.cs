995c1a75efa0578aa91f9b9f84cada4b64ef0aec
Shaiv <antoni.labisz@wp.pl>
2023-03-12 08:55:19 +0100

    Cleaning

--- XboxMouseKeyboardEmulator/MouseMove.cs
+++ XboxMouseKeyboardEmulator/MouseMove.cs
@@ -1,9 +1,4 @@
 ﻿using SharpDX.XInput;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WindowsInput;
 
 namespace XboxMouseKeyboardEmulator
